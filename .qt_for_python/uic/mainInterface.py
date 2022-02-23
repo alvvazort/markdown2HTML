d36ecc3600230dfcfa60a93e6a3945a7630225c8
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2022-02-24 00:02:25 +0100

    Interfaz funcionando

---
+++ .qt_for_python/uic/mainInterface.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainInterface.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_ImageViewerWindow(object):
+    def setupUi(self, ImageViewerWindow):
+        if not ImageViewerWindow.objectName():
+            ImageViewerWindow.setObjectName(u"ImageViewerWindow")
+        ImageViewerWindow.resize(1080, 800)
+        self.centralwidget = QWidget(ImageViewerWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        icon = QIcon()
+        icon.addFile(u"../icons/Save-icon.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setIconSize(QSize(25, 25))
+
+        self.horizontalLayout.addWidget(self.pushButton)
+
+        self.saveButton = QPushButton(self.centralwidget)
+        self.saveButton.setObjectName(u"saveButton")
+        icon1 = QIcon()
+        icon1.addFile(u"../icons/md-to-html.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.saveButton.setIcon(icon1)
+        self.saveButton.setIconSize(QSize(50, 25))
+
+        self.horizontalLayout.addWidget(self.saveButton)
+
+        self.currentFile = QLabel(self.centralwidget)
+        self.currentFile.setObjectName(u"currentFile")
+        sizePolicy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.currentFile.sizePolicy().hasHeightForWidth())
+        self.currentFile.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout.addWidget(self.currentFile)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.imageLabel = QLabel(self.centralwidget)
+        self.imageLabel.setObjectName(u"imageLabel")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.imageLabel.sizePolicy().hasHeightForWidth())
+        self.imageLabel.setSizePolicy(sizePolicy1)
+
+        self.verticalLayout.addWidget(self.imageLabel)
+
+        ImageViewerWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(ImageViewerWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        ImageViewerWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(ImageViewerWindow)
+
+        QMetaObject.connectSlotsByName(ImageViewerWindow)
+    # setupUi
+
+    def retranslateUi(self, ImageViewerWindow):
+        ImageViewerWindow.setWindowTitle(QCoreApplication.translate("ImageViewerWindow", u"Example Image Viewer", None))
+        self.pushButton.setText("")
+        self.saveButton.setText("")
+        self.currentFile.setText(QCoreApplication.translate("ImageViewerWindow", u"No file selected", None))
+        self.imageLabel.setText("")
+    # retranslateUi
+
--- app/run_pyqt.py
+++ app/run_pyqt.py
@@ -1,62 +1,25 @@
 import sys
 
-from PyQt5 import QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication
-import markdown
-
-import image_viewer_pyqt
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
 
 
-class ImageViewer(QtWidgets.QMainWindow, image_viewer_pyqt.Ui_ImageViewerWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setupUi(self)
-        self.openImageButton.clicked.connect(self.open_image_action)
+class Markdown2HTML(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('mainInterface.ui', self) #Carga la interfaz (Hecha en Qt designer)
 
-    def open_image_action(self):
-        print("button clicked")
-
-    def show_image_picker(self):
-        picker = ImagePicker(self).get_widget()
-        picker.fileSelected.connect(self.image_file_selected)
-        picker.show()
-        return picker
-
-    def image_file_selected(self, file):
-        self.currentFile.setText(file)
-        picture = QtGui.QPixmap(file)
-        self.imageLabel.setPixmap(picture)
-
-
-class ConfirmationPrompt:
-    def __init__(self, ui):
-        self.ui = ui
-
-    def get_widget(self):
-        widget = QtWidgets.QMessageBox(self.ui)
-        widget.setText('Do you really want to open an image?')
-        widget.setIcon(widget.Question)
-        widget.addButton('Yes', widget.AcceptRole)
-        widget.addButton('No', widget.RejectRole)
-        widget.setDetailedText('This is just a show-off for OS differences in prompt windows')
-        return widget
-
-
-class ImagePicker:
-    def __init__(self, ui):
-        self.ui = ui
-
-    def get_widget(self):
-        picker = QtWidgets.QFileDialog(self.ui)
-        picker.setMimeTypeFilters(['image/jpeg', 'image/png', 'image/bmp', 'image/tiff', 'image/gif', 'image/webp'])
-        return picker
-
+    
 
 def main():
     app = QApplication(sys.argv)
-    form = ImageViewer()
+    form = Markdown2HTML() #Crea la ventana de la aplicación
     form.show()
-    app.exec_()
+    
+    try:
+        sys.exit(app.exec_())
+    except SystemExit:
+        print('Closing Window')
 
 
 if __name__ == '__main__':
